ff3312e999b718378994f5d572198a0f6bbeb730
Brahian Monsalve Muñoz <brahianmonsalve412@gmail.com>
2025-09-09 19:48:08 +0200

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { SelectModule } from 'primeng/select';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, ButtonModule, SelectModule],
+    templateUrl: './app.html',
+    styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('codequest-blog-app');
+    protected readonly title = signal('codequest-blog-app');
 }
